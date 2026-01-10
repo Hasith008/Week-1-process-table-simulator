539e61c42ba8faab05fb268f5364f74ab4671333
Hasith008 <hasithnadupuru2008@gmail.com>
2026-01-10 10:06:52 +0530

    Add files via upload

---
+++ Project.cpp
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct
+{
+	int id;
+	char pName[20];
+	int priority;
+	float BT; 
+}Process;
+int count=0;
+Process table[50];
+void Add()
+{
+	printf("Enter process-&d details\n",count+1);
+	printf("Enter Pid\n");
+	scanf("%d",&table[count].id);
+	printf("Enter process name\n");
+	scanf("%s",table[count].pName);
+	printf("Enter priority\n");
+	scanf("%d",&table[count].priority);
+	printf("Enter BT\n");
+	scanf("%f",&table[count].BT);
+	count++;
+}
+void Display()
+{
+	int i;
+	for(i=0;i<=count;i++)
+	{
+	printf("\nprocess-%d\n",i+1);
+	printf("\nPid=%d\tPname=%s\tpriority=%d\tBT=%f\n",table[i].id,table[i].pName,table[i].priority,table[i].BT);
+	}
+}
+void BubbleSortByPriority()
+{
+	int i,j;
+	for(i=0;i<count;i++)
+	{
+		for(j=0;j<count-i-1;j++)
+		{
+			if(table[j].priority>table[j+1].priority)
+			{
+				int temp=table[j].priority;
+				table[j].priority=table[j+1].priority;
+				table[j+1].priority=temp;
+			}
+		}
+	}
+}
+void InsertionSortByPid()
+{
+	
+}
+void ShellSortByPid()
+{
+	
+}
+void LinearSearchByPid()
+{
+	
+}
+void BinarySearchByPid()
+{
+	
+}
+main()
+{
+	while(1){
+	printf("1.Add\n2.Display\n3.BubbleSortByPriority\n4.InsertionSortByPid\n5.ShellSortByBT\n6.LinearSearchByPid\n7.BinarySearchByPid\n");
+	int choice;
+	printf("Enter your choice");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:Add();break;
+		case 2:Display();break;
+		case 3:BubbleSortByPriority();break;
+		case 4:InsertionSortByPid();break;
+		case 5:ShellSortByPid();break;
+		case 6:LinearSearchByPid();break;
+		case 7:printf("come here after sorting");
+				BinarySearchByPid();break;
+		case 8:exit(0);break;
+	}
+}
+}
